f97f0ea227c740e160f738cfda7efb133f97e6a1
Poojan Thummer <poojanwork101@gmail.com>
2026-05-29 19:09:01 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+
+#------variables------- 
+
+""" Variables are used as a storage to store things in 
+python """
+
+name = "Poojan Thummer" #-> variable then  value
+Gender = "Male"         #-> variable then  value
+age = "23"              #-> variable then  value
