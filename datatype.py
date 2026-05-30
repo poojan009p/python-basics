be9f95219ea26fe4123b602d5e720aea67ccd8c4
Poojan Thummer <poojanwork101@gmail.com>
2026-05-30 11:19:51 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,38 @@
+
+"""---------DATA TYPES--------"""
+
+#-----------------------------------------------------------------------------------------------------------------------------------"
+
+
+"---Integer---" # All the numbers excluding decimal places and fraction
+X = 10  # -> value in integer form
+
+#-----------------------------------------------------------------------------------------------------------------------------------"
+
+
+"---Float---" # All the decimal numbers and fraction values are Float.
+
+price = 99.5    # value in Decimal 
+price = 18/2    # 9.0 is also Float
+
+#-----------------------------------------------------------------------------------------------------------------------------------"
+
+
+"---Complex---" # Numbers with real and imaginary parts are complex.
+
+value = 3j
+
+#-----------------------------------------------------------------------------------------------------------------------------------"
+
+"---String---" # This is used to store anything in python,  literally anything that are available on your keyboard.
+
+name = "Poojan"
+poojan = "992002"
+poojan = """!@#$%^&*()_+/-+"""
+
+#-----------------------------------------------------------------------------------------------------------------------------------"
+
+"---Boolean---"  # always give the result of True and False.
+
+learning = True
+nolearning = False
