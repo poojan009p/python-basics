62f34c989779666364a9aa9e6be4d66b075f4d14
poojan009p <poojanwork101@gmail.com>
2026-06-01 15:38:44 +0530

    type_conversions

---
+++ type_conversion.py
@@ -0,0 +1,17 @@
+# All about type conversion
+
+int()
+float()
+str()
+bool()
+ 
+"-------------------------------------------------------------"
+# into integer 
+
+a="12" # value in string
+
+a=int(a) # converted into integer
+
+print(type(a))
+
+"-------------------------------------------------------------"
