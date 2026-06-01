336d89b4e340ea6b335435aa2d6c33153c998c72
poojan009p <poojanwork101@gmail.com>
2026-06-01 15:45:02 +0530

    .

--- type_conversion.py
+++ type_conversion.py
@@ -10,8 +10,20 @@ bool()
 
 a="12" # value in string
 
-a=int(a) # converted into integer
+a=int() # converted into integer
 
-print(type(a))
+print(type(a)) #= type int
 
 "-------------------------------------------------------------"
+# into float 
+
+b = 12   # value in integer
+
+b = float() # converted into float
+
+print(type(b)) #= type float
+
+"-------------------------------------------------------------"
+# into float 
+
+
