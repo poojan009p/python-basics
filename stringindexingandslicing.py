f8b943d35484c77248eedb0c532836d82a09b137
poojan009p <poojanwork101@gmail.com>
2026-06-01 17:07:06 +0530

    stringindexingandslicing

--- stringindexingandslicing.py
+++ stringindexingandslicing.py
@@ -18,3 +18,21 @@ print(name[-1]) #n
 
 "--------------------------------------------------------------"
 "---String Slicing---"
+
+text = "PoojanThummer"
+
+print(len(text))    #gives length of string 
+print(text[0:6])    #Poojan
+print(text[6:13])   #Thummer
+print(text[:6])     #Poojan
+print(text[6:])     #Thummer
+print(text[::-1])   #remmuhTnajooP
+
+print(text.upper())  # Make Uppercase everything
+print(text.lower())  # Make Lowercase everything
+print(text.capitalize()) # Capitalize everything
+print(text.replace("PoojanThummer","Poojan"))  # replace PoojanThummer -> Poojan
+
+# End of Program
+# Thank You
+# Program Finished
