821f4c532fb1c3771bc48b99a2233aa36376dd82
poojan009p <poojanwork101@gmail.com>
2026-06-01 16:45:45 +0530

    stringindexingandslicing

--- stringindexingandslicing.py
+++ stringindexingandslicing.py
@@ -4,6 +4,17 @@ Author: Poojan
 Description: Basic examples of Python String indexing and Slicing.
 
 """
+"--------------------------------------------------------------"
+"---String Indexing---"
 
-a = (1,2,3,4,5,6,7,8,9)
+name = "poojan"
 
+print(name[0])  #p
+print(name[1])  #o
+print(name[2])  #o      # Positive indexing starts from 0
+print(name[-3]) #j      # Negative indexing starts from -1
+print(name[-2]) #a  
+print(name[-1]) #n
+
+"--------------------------------------------------------------"
+"---String Slicing---"
