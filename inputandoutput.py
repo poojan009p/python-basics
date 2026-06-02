9c7bd0d542780888df3b107f49104743632e0488
poojan009p <poojanwork101@gmail.com>
2026-06-02 10:58:56 +0530

    input and output

---
+++ inputandoutput.py
@@ -0,0 +1,29 @@
+"""
+Topic: inout and output
+Author: Poojan
+Description: Basic examples of Python input and output.
+
+"""
+
+"--------------------------------------------------------------------------------------------------------------------------"
+#output
+
+name = "poojan"
+
+print(name)
+
+# There is no other functions to provide the result on the terminal we just have to use print() function
+
+"--------------------------------------------------------------------------------------------------------------------------"
+#input
+
+name = input("Provide Your name - ")
+
+# input() is used to take input from users
+
+print(name)
+
+
+# End of Program
+# Thank You
+# Program Finished
