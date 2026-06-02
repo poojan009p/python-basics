8adc706679d861cd6bce83da1c71c6cde18b5d01
poojan009p <poojanwork101@gmail.com>
2026-06-02 17:04:43 +0530

    updates

---
+++ 3.collectiondatatype.py
@@ -0,0 +1,52 @@
+"""
+Topic: Collection Data Types
+Author: Poojan
+Description: Basic examples of Python data types.
+
+"""
+
+#-----------------------------------------------------------------------------------------------------------------------------------"
+
+"---List---" # All the numbers excluding decimal places and fraction
+
+number = [1,2,3,4,5,6,7,8,9] # syntax[]
+                             # Mutable,Duplicates,Ordered,Heterogenous
+
+#-----------------------------------------------------------------------------------------------------------------------------------"
+
+"---Tuple---" # All the numbers excluding decimal places and fraction
+
+number = (1,2,3,4,5,6,7,8,9) # syntax()
+                             # Immutable,Duplicates,Ordered,Heterogenous
+
+#-----------------------------------------------------------------------------------------------------------------------------------"
+
+"---Set---" # All the numbers excluding decimal places and fraction
+
+number = {1,2,3,4,5,6,7,8,9} # syntax{}
+                             # Immutable,Duplicates,Unordered,Heterogenous
+
+#-----------------------------------------------------------------------------------------------------------------------------------"
+
+"---Dictionary---" # Stores data as key-value pairs.
+
+person = {"name": "Poojan",} # syntax
+                             # Mutable,Duplicates,Ordered,Heterogenous
+print(person["name"])
+
+
+"""
+    Data Type-----Example
+    int	             9
+    float       	9.5
+    str	            "Hello"
+    bool        	True
+    list        	[1,2,3]
+    tuple	        (1,2,3)
+    set         	{1,2,3}
+    dict        	{"name":"Poojan"}
+"""
+
+# End of Program
+# Thank You
+# Program Finished
