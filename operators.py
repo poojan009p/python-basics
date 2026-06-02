58ff9aa450907caeb42109af1532147312a22770
poojan009p <poojanwork101@gmail.com>
2026-06-02 16:57:29 +0530

    operators

--- operators.py
+++ operators.py
@@ -26,3 +26,53 @@ floor division  - //
 modulus         - %
 exponential     - **
 """
+
+a=9
+b=9
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+
+"--------------------------------------------------------------------------------------------"
+"---Assignment operators---"
+
+# basic assignment operator is =
+
+"---Compound assignment operators---"
+# it combines arithmetic operator with assignment 
+
+"""
++=          - add and assign    
+-=          - subract and assign 
+*=          - multiply and assign
+/=          - divide and assign
+//=         - floor divide and assign
+**=         - exponential and assign
+%=          - modulus and assign
+"""         
+
+"--------------------------------------------------------------------------------------------"
+"---Comparison operators---"
+# also called relational operator,are used to compare two values
+# comparision operators always provide boolean results true or false 
+
+"""
+==      - equals to
+!=      - not equals to
+>       - greater than
+<       - less than
+>=      - greater than or equal to
+<=      - less than or equal to
+""" 
+
+"---Logical operators---"
+
+# and - returs true if both conditions re true
+# or    - returs true at least one condition is true.
+# not   - returns the boolean value.
+
