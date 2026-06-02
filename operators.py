ee2f930725e7b155a6aeb0d860a19f5c0f32b3b5
poojan009p <poojanwork101@gmail.com>
2026-06-02 11:04:39 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,19 @@
+"""
+Topic: Operators
+Author: Poojan
+Description: Basic examples of Python Operators.
+"""
+
+# Operators are symbols that perform operations on variables 
+# and values. Python has several types of operators for 
+# different tasks like arithmetic, comparison, logical operations, 
+# and more
+
+"--------------------------------------------------------------------------------------------"
+"---Arithmetic operators---"
+
+# Arithmetic operators perform mathematical operations like 
+# addition, subtraction, multiplication, division, etc
+
+# there are total 7 types of arithmetic operators
+
