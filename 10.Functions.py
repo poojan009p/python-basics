a561d4e1ca3da9575ff1b51ca4e34888d28d5677
poojan009p <poojanwork101@gmail.com>
2026-06-04 16:21:29 +0530

    Functions

--- 10.Functions.py
+++ 10.Functions.py
@@ -2,4 +2,31 @@
 Topic: Functions
 Author: Poojan
 Description: Basic examples of Functions.
-"""     
+""" 
+
+"----------------------------------------------------------------------------------------------------------------"
+"---Functions---"
+# Functions means reusable code into a block that can be executed by calling the function name.
+
+# There are many in-built functions in python like - print(), input() len() etc.
+# we can create our own function also.
+
+def poojan():                   #def is used to create function.              
+    print("My name is Poojan")
+
+poojan()            #use of function.
+
+"---Function Parameter and arguments---"
+
+# parameters are variable listed inside the function definition.
+# arguments are value passed to a function when it is called. 
+
+def name(yourname): #your name is a parameter
+    print(f"hello,{yourname}!")
+
+name("poojanThummer")
+
+"----------------------------------------------------------------------------------------------------------------"
+"---Types of argument---"
+
+
