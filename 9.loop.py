f423d5b3c09c65b91328bd0de0b897726bda0e1c
poojan009p <poojanwork101@gmail.com>
2026-06-04 09:10:14 +0530

    loops

--- 9.loop.py
+++ 9.loop.py
@@ -3,3 +3,37 @@ Topic: Loops
 Author: Poojan
 Description: Basic examples of Loops.
 """
+"---Loops---"
+# Loops - Loops in python allow us to execute block of code multiple times without rewriting it.
+
+"""---Types of loops---"""
+    # for loop
+    # while loop
+
+"---For loop---"
+
+#  range() - it is used to generate a sequence of numbers , which is commonly used in loops.
+
+# for numbers
+
+for i in range(10):
+    print(i)
+
+# for string
+
+p = "poojan"
+
+for i in range(len(p)):
+    print(i)
+
+# other method - this method gives direct access to characters instead of index values
+
+for char in p:
+    print(char)
+
+"---While loop---"
+
+#  it repeats the loop as long as condition is True. It is useful when the number of iteration is unknown.
+
+
+
