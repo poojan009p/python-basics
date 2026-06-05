b8e1b7b873ab050f4f84ea0bb1d5958407540475
poojan009p <poojanwork101@gmail.com>
2026-06-05 17:00:10 +0530

    ifelse

--- If-elsepractice.py
+++ If-elsepractice.py
@@ -33,7 +33,7 @@ Write a program that takes a number and prints:
 “Zero” if number == 0"""
 "------------------------------------------------------------------------------------------------------------"
 
-# num2 = int(input("Provide your number"))
+# num2 = int(input("Provide your number - "))
 
 # if num2==0:
 #     print(f"{num2} is zero")
@@ -46,7 +46,37 @@ Write a program that takes a number and prints:
 
 "------------------------------------------------------------------------------------------------------------"
 
+"""3. Voting Eligibility
 
+Write a program that takes age as input and checks:
+
+If age ≥ 18 → “Eligible to vote”
+Else → “Not eligible to vote”"""
+
+"------------------------------------------------------------------------------------------------------------"
+
+# age = int(input("Provide your age - "))
+
+# if age >= 18:
+#     print("You are eligible for Voting")
+# else:
+#     print("You are not eligible")
+
+"------------------------------------------------------------------------------------------------------------"
+"""4. Largest of Two Numbers
+
+Take two numbers as input and print which one is greater, or if both are equal."""
+"------------------------------------------------------------------------------------------------------------"
+
+a1 = int(input("Provide first number -  "))
+a2 = int(input("Provide second number -  "))
+
+if a1>a2:
+    print(f"{a1} is greater than {a2}")
+elif a2>a1:
+    print(f"{a2} is greater than {a1}")
+elif a1==a2:
+    print(f"{a1} {a2} both are equal")
 
 
 
