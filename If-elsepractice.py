d4db96c394abbded85b48b35662a7ff8dcebc63f
poojan009p <poojanwork101@gmail.com>
2026-06-05 16:53:45 +0530

    ifelse

--- If-elsepractice.py
+++ If-elsepractice.py
@@ -13,14 +13,36 @@ print("===================================")
 """ 1. Even or Odd
 
  Write a program that takes a number as input and checks whether it is even or odd."""
+"------------------------------------------------------------------------------------------------------------"
 
-num1 = int(input("Provide Your first number - "))
+# num1 = int(input("Provide Your first number - "))
 
 
-if num1%2==0 :
-    print(f"{num1} is even")
-else:
-    print(f"{num1} is odd")
+# if num1%2==0 :
+#     print(f"{num1} is even")
+# else:
+#     print(f"{num1} is odd")
+
+"------------------------------------------------------------------------------------------------------------"
+"""2-Positive, Negative, or Zero
+
+Write a program that takes a number and prints:
+
+“Positive” if number > 0
+“Negative” if number < 0
+“Zero” if number == 0"""
+"------------------------------------------------------------------------------------------------------------"
+
+# num2 = int(input("Provide your number"))
+
+# if num2==0:
+#     print(f"{num2} is zero")
+
+# elif num2>0:
+#     print(f"{num2} is positive")
+
+# elif num2<0:
+#     print(f"{num2} is negative")
 
 "------------------------------------------------------------------------------------------------------------"
 
@@ -33,10 +55,8 @@ else:
 
 
 
-
-
-print("===================================")
-print("   Thank you for practicing today  ")
-print("   Keep coding, keep growing 🚀     ")
-print("   See you in the next session!     ")
-print("===================================")
+# print("===================================")
+# print("   Thank you for practicing today  ")
+# print("   Keep coding, keep growing 🚀     ")
+# print("   See you in the next session!     ")
+# print("===================================")
