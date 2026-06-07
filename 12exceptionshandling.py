ddf40847f9b959183f257f27203f90cff94e778f
poojan009p <poojanwork101@gmail.com>
2026-06-07 11:58:35 +0530

    exception handling

--- 12exceptionshandling.py
+++ 12exceptionshandling.py
@@ -3,3 +3,12 @@ Topic: excepion handling
 Author: Poojan
 Description: Basic examples of Python excepion handling.
 """
+"-----------"
+"errors"
+"""
+there are two type of errors such as 
+
+syntax error 
+indentation error
+
+"""
